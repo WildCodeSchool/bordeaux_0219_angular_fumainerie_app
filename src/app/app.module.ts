4d078a9cef17c21d978e2583186102837a53e0ad
Adrien Bouissier <adrien.bouissier@gmail.com>
2019-12-03 10:09:46 +0100

    feat:contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './shared/app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './app/components/accueil/contact/contact.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
