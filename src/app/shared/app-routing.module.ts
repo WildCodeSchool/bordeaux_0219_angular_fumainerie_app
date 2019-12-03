3fa3959b2f46db1220f947645dd9653052a4cf6f
Julie Hellec <hellecjulie@gmail.com>
2019-12-03 11:11:48 +0100

    Feat: path route created for underconsctruction, add image for that

--- src/app/shared/app-routing.module.ts
+++ src/app/shared/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UnderConstructionComponent } from '../Pages/under-construction/under-construction.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'En Construction' },
+  { path: 'En Construction', component: UnderConstructionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
