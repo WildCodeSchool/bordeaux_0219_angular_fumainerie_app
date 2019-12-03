d3cc4c56ddf90a580defaa1639569af12a9f36bd
Delache <delache33@gmail.com>
2019-12-03 10:47:41 +0100

    feat: createDirectiveItem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './components/accueil/contact/contact.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { CarouselItemDirective } from './shared/directives/carousel-item.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     CarouselComponent,
+    CarouselItemDirective,
   ],
   imports: [
     BrowserModule,
