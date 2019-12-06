4b6d9f1dc761e9e2c38aa79e7fb19062e02210a4
Julie Hellec <hellecjulie@gmail.com>
2019-12-06 15:53:32 +0100

    Feat: Amorce liens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/accueil/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'accueil'},
+  { path: 'accueil', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
