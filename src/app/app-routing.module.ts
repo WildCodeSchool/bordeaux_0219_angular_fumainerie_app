5381ae842ebda6c4715b86542a8038f4fe4f9848
Adrien Bouissier <adrien.bouissier@gmail.com>
2019-12-11 11:12:43 +0100

    fix:routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', redirectTo: '/accueil', pathMatch: 'full'},
   {path: 'accueil', component : HomepageComponent},
   {path: 'connexion', component  : SignInComponent},
   {path: 'actualites', component : ActualitesComponent},
