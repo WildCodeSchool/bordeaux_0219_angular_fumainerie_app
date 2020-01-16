07405f04bf1b8c5106ab8d199679b3c836383c47
Delache <delache33@gmail.com>
2020-01-16 22:49:55 +0100

    feat: newsletterPost

--- src/app/components/accueil/contact/contact.component.ts
+++ src/app/components/accueil/contact/contact.component.ts
@@ -1,10 +1,10 @@
-
-import { NewsletterService } from './../../../shared/services/newsletter.service';
+import { NewsletterModalComponent } from './../../modals/newsletter-modal/newsletter-modal.component';
 import { Component, OnInit } from '@angular/core';
 import { emailValidator } from '../../../shared/validators/email-validator';
-import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 import { MatDialog} from '@angular/material';
 import { HttpClient, HttpParams} from '@angular/common/http';
+import { stringify } from 'querystring';
 
 interface MailChimpResponse {
   result: string;
@@ -22,23 +22,23 @@ export class ContactComponent implements OnInit {
   error = '';
 // <form action="https://gmail.us4.list-manage.com/subscribe/post-json?u=46dee92b175ee958c07343421&amp;id=8aa6f29244"
   constructor(private http: HttpClient,
-              private formBuilder: FormBuilder,
               public dialog: MatDialog) { }
 
-  mailForm: FormGroup;
 
-  ngOnInit() {
-    this.mailForm = this.formBuilder.group({
-      EMAIL: ['', [Validators.required, emailValidator]]
-    });
+  mailForm = new FormGroup({
+    EMAIL: new FormControl('', [Validators.required, emailValidator]),
+  });
+
+
+ngOnInit() {
   }
 
-  onSubmit() {
-      this.error = '';
-      if (this.mailForm.status === 'VALID' && this.mailForm.status === 'VALID') {
+onSubmit() {
+  this.error = '';
+  if (this.mailForm.status === 'VALID' && this.mailForm.status === 'VALID') {
 
       const params = new HttpParams()
-        .set('EMAIL', this.mailForm.value)
+        .set('EMAIL', (this.mailForm.get('EMAIL').value))
         .set('b_46dee92b175ee958c07343421_8aa6f29244', ''); // hidden input name
 
       const mailChimpUrl = this.mailChimpEndpoint + params.toString();
@@ -54,16 +54,13 @@ export class ContactComponent implements OnInit {
         console.error(error);
         this.error = 'Sorry, an error occurred.';
       });
-
-
-
-   /*    this.newsletterService.addEmail(this.mailForm.value).subscribe();
       const dialogRef = this.dialog.open(NewsletterModalComponent, {
         width: '50%'
       });
       dialogRef.afterClosed().subscribe(result => {
         console.log('The dialog was closed');
-      }); */
+        this.mailForm.reset();
+      });
   }
 }
 }
