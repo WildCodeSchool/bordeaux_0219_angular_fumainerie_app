a3da7d582d8f1885a7d1280610e5f8c075b8e681
DOligex <olinours@yahoo.fr>
2020-01-22 11:57:05 +0100

    fixe: duplicate import solve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,7 +61,6 @@ import { QuestionDeleteModalComponent } from './components/modals/question-delet
 import { NewsDeleteModalComponent } from './components/modals/news-delete-modal/news-delete-modal.component';
 import { NewsFormComponent } from './components/forms/news-form/news-form.component';
 import { NewsModalComponent } from './components/modals/news-modal/news-modal.component';
-import { DrainingComponent } from './components/forms/draining/draining.component';
 
 
 
