1c69c055d03643a5d0ce25a79753f03a982a8b33
MrWhite <hugoblanc.blend@gmail.com>
2020-01-23 10:24:00 +0100

    fix: confilct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './shared/app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/accueil/contact/contact.component';
-import { UnderConstructionComponent } from './Pages/under-construction/under-construction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent,
-    UnderConstructionComponent
+    ContactComponent
   ],
   imports: [
     BrowserModule,
