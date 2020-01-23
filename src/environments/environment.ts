a9b69ea175f10af86cad87ea147cb1853a956472
Adrien Bouissier <adrien.bouissier@gmail.com>
2020-01-23 17:29:37 +0100

    style: responsive css homepage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // url: 'http://localhost:3000'
-  url: 'https://db-db.lafumainerie.com'
+  url: 'http://localhost:3000'
+  // url: 'https://db-db.lafumainerie.com'
 };
 
 /*
