3c891396f1f5c2b000343d6e3181aa287aa822e3
Adrien Bouissier <adrien.bouissier@gmail.com>
2020-01-27 14:08:32 +0100

    chore: debug

--- src/app/core/log.interceptor.ts
+++ src/app/core/log.interceptor.ts
@@ -15,7 +15,6 @@ export class LogInterceptor implements HttpInterceptor {
     if (!token) {
       return next.handle(req);
     }
-    console.log(localStorage.getItem('JWT-TOKEN'));
     const headers = req.headers.set('Authorization', `Bearer ${token}`);
     const authReq = req.clone({headers});
     return next.handle(authReq);
