2d1fc891d4ded7cdbf95ffd2cf81b533081a4239
DOligex <olinours@yahoo.fr>
2020-01-27 10:54:27 +0100

    feat: implement the user: User inside all models

--- src/app/shared/models/gouvernance-personality.ts
+++ src/app/shared/models/gouvernance-personality.ts
@@ -1,9 +1,13 @@
+import { User } from './user';
+
 export class GouvernancePersonality {
   public id: number;
   public name: string;
   public picture: string;
   public details: string;
   public citation: string;
+  public user?: User;
+
 
   constructor(input?: GouvernancePersonality) {
     Object.assign(this, input);
