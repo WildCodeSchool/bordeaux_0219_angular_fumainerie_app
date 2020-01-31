6d1c5ab49f6cba5a57d76ab2a428a2e074f0dfed
Adrien Bouissier <adrien.bouissier@gmail.com>
2020-01-31 09:40:50 +0100

    chore: synthaxe

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SignupService } from 'src/app/shared/services/signup.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,8 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder,
+              private service: SignupService,
+              private router: Router) { }
+
+              signUpForm: FormGroup;
+
 
   ngOnInit() {
+    this.signUpForm = this.formbuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      birth_date: ['', Validators.required]
+    });
   }
+  onSignUp() {
+    this.service.postNewUser(this.signUpForm.value).subscribe();
+  }
+
+  goToSignIn() {
+    this.router.navigate(['/connexion']);
+  }
+
 }
